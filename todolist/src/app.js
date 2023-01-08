c5adc3cc7993dc037dece4fc36cf5a350a5cab1d
odcenteno <pixie_cemodan@hotmail.com>
2023-01-07 21:10:31 -0700

    work todo list with EJS layouts

--- todolist/src/app.js
+++ todolist/src/app.js
@@ -10,30 +10,64 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-const taskList = ['Sunday'];
+const taskList = [];
+const taskWork = [];
+
+const today = new Date();
+const options = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long'
+}
+const currentDay = today.toLocaleDateString('es-MX', options);
 
 app.get('/', (req, res) => {
-  const today = new Date();
-  const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  }
-  const currentDay = today.toLocaleDateString('es-MX', options);
-
   res.render('list', {
+    pageView: 'list',
     dayToDisplay: currentDay || 'Error getting the current day',
     taskList: taskList
   });
 });
 
+app.get('/work', (req, res) => {
+  res.render('work', {
+    pageView: 'work',
+    dayToDisplay: currentDay || 'Error getting the current day',
+    taskWork: taskWork
+  })
+});
+
 app.post('/', (req, res) => {
   console.log(req.body.newTask);
+  const pageBtn = req.body.pageView;
+  console.log(pageBtn);
   const newTask = req.body.newTask;
+
   if (newTask === ""){
     console.log('error: empty string');
-  } else { taskList.push(newTask) };
-  res.redirect('/');
+  } else if (pageBtn === 'list') {
+    taskList.push(newTask);
+    res.redirect('/');
+  } else if (pageBtn === '') {
+    taskWork.push(newTask);
+    res.redirect('/work');
+  };
+});
+
+app.post('/work', (req, res) => {
+  const pageBtn = req.body.pageView;
+  console.log(pageBtn);
+  const newTask = req.body.newTask;
+
+  if (newTask === ""){
+    console.log('error: empty string');
+  } else if (pageBtn === 'list') {
+    taskList.push(newTask);
+    res.redirect('/');
+  } else if (pageBtn === 'work') {
+    taskWork.push(newTask);
+    res.redirect('/work');
+  };
 });
 
 app.listen(PORT, () => {
