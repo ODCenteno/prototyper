364e24ad60a3ca3d529c35f7c02ee80123fd628a
odcenteno <pixie_cemodan@hotmail.com>
2023-01-08 17:19:12 -0700

    routing for single post page and home resume

--- ejs-blog/app.js
+++ ejs-blog/app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const lodash = require('lodash');
 const port = process.env.PORT;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const posts = [];
 
 const app = express();
 
@@ -19,6 +21,7 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
   res.render('home', {
     startingContent: homeStartingContent,
+    posts: posts
   });
 });
 app.get('/about', (req, res) => {
@@ -33,7 +36,19 @@ app.get('/contact', (req, res) => {
 });
 app.get('/compose', (req, res) => {
   res.render('compose', {
+  });
+});
 
+app.get('/posts/:postTitle', (req, res) => {
+  console.log(req.params.postTitle);
+  const urlParam = lodash.kebabCase(req.params.postTitle);
+  posts.forEach(post => {
+    if (post.routeParam === urlParam) {
+      res.render(`post`, {
+        title: post.title,
+        content: post.content
+      });
+    };
   });
 });
 
@@ -41,7 +56,19 @@ app.post('/compose', (req, res) => {
   const newTitle = req.body.newPostTitle;
   const postContent = req.body.newPostContent;
   console.log(newTitle, postContent);
-})
+
+  const route = lodash.kebabCase(newTitle);
+  // const route = newTitle.toLowerCase().trim().replaceAll(' ', '-');
+
+  const post = {
+    title: newTitle,
+    content: postContent,
+    routeParam: route
+  };
+  console.log(post);
+  posts.push(post);
+  res.redirect('/');
+});
 
 
 
