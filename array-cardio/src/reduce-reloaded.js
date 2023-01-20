d6ede0e939b73c7193f94336718dc64f8452f542
odcenteno <pixie_cemodan@hotmail.com>
2023-01-19 23:52:55 -0700

    trying to get a good json

--- array-cardio/src/reduce-reloaded.js
+++ array-cardio/src/reduce-reloaded.js
@@ -1,1 +1,15 @@
 const items = [1, 3, 2, 3];
+
+json = {
+  "questions":
+    [
+      {
+        "question": "¿Qué se debe hacer para devolverse al inicio del curso de Platzi?", "answers": [
+          {"content": "Crear un nuevo repositorio", "correct": False},
+          {"content": "Actualizar el home page", "correct": False},
+          {"content": "Cambiar el nombre del repositorio", "correct": False},
+          {"content": "Empezar desde cero en la rama por defecto", "correct": True}
+        ]
+    }
+  ]
+}
