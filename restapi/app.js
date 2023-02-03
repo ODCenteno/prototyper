7ed67e88c93260ccb270eeb1602a7f40af78e662
odcenteno <pixie_cemodan@hotmail.com>
2023-02-02 22:52:40 -0700

    getting michis using fetch

--- restapi/app.js
+++ restapi/app.js
@@ -1,24 +1,46 @@
-const imgDOM = document.querySelector('.kitty-image');
+// require('dotenv').config()
 
-const API_URL = "https://randomfox.ca/floof/";
+// const MICHI_API_KEY = process.env.API_KEY
 
+const imgDOM1 = document.getElementById('img1');
+const imgDOM2 = document.getElementById('img2');
 
-async function reload() {
+const imagesDOM = [imgDOM1, imgDOM2];
+const imgSources = [];
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=2";
+const API_URL_FAV = "https://api.thecatapi.com/v1/favourites?limit=2";
+
+function domify() {
+  imagesDOM.forEach((image, i) => {
+      image.src = imgSources[i];
+  });
+}
+
+async function callAPI() {
   const callFoxAPI = await fetch(API_URL);
   const foxData = await callFoxAPI.json();
-  const imgFox = foxData.image;
-  imgDOM.src = imgFox;
-  return imgFox;
+  foxData.forEach(data => {
+    imgSources.push(data.url);
+  })
 }
-  // .then(res =>
-  //     res.json())
-  //   .then(data => {
-  //     console.log(data);
-  //     const imgDOM = document.querySelector('.kitty-image');
-  //     imgDOM.src = data.image;
-  //   })
 
-  // Retos: cambiar la sintaxis por async await
-  // agregar un botón y los eventos para recargar una nueva imagen al hacer clic
+async function reload() {
+  try {
+    for (let i = 0; i < imagesDOM.length; i++) {
+      await callAPI();
+    }
+    console.log(imgSources);
+    domify();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function loadFavourites() {
+  const res = await fetch(API_URL_FAV);
+  const data = await res.json();
+  console.table(data)
+}
 
 reload();
+loadFavourites()
