5d20a0730cc2bc9793c8a8956254aa48a601eac2
odcenteno <pixie_cemodan@hotmail.com>
2023-02-04 15:57:10 -0700

    Save error in VSCODE

--- restapi/app.js
+++ restapi/app.js
@@ -6,21 +6,22 @@ const imgDOM1 = document.getElementById('img1');
 const imgDOM2 = document.getElementById('img2');
 
 const imagesDOM = [imgDOM1, imgDOM2];
-const imgSources = [];
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=2";
-const API_URL_FAV = "https://api.thecatapi.com/v1/favourites?limit=2";
+let imgSources = [];
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_NO3mp7zCj5AXEjbULlOQYt0b4wsE2GR8Jux75bIk1LuM2HBAlpMzOdrQQ3QCQU8s`";
+const API_URL_FAV = `https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_NO3mp7zCj5AXEjbULlOQYt0b4wsE2GR8Jux75bIk1LuM2HBAlpMzOdrQQ3QCQU8s`;
 
 function domify() {
   imagesDOM.forEach((image, i) => {
-      image.src = imgSources[i];
+      image.src = imgSources[i].url;
   });
 }
 
 async function callAPI() {
-  const callFoxAPI = await fetch(API_URL);
+  imgSources = [];
+  const callFoxAPI = await fetch(API_URL_RANDOM);
   const foxData = await callFoxAPI.json();
   foxData.forEach(data => {
-    imgSources.push(data.url);
+    imgSources.push({url: data.url, id: data.id});
   })
 }
 
@@ -40,7 +41,23 @@ async function loadFavourites() {
   const res = await fetch(API_URL_FAV);
   const data = await res.json();
   console.table(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
 }
 
-reload();
+async function saveFavourite() {
+  const res = await fetch(API_URL_FAV, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id
+    })
+  })
+}
+
+reload( imgSources );
 loadFavourites()
